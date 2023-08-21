9f0debc493d3e0b813af4fa129bb21b689534b28
siddu9900 <siddu.xworkz@gmail.com>
2023-08-21 12:07:24 +0530

    Array

---
+++ SoundBar.java
@@ -0,0 +1,112 @@
+/*class SoundBar{
+
+
+	public static void main(String args[]){
+	
+
+				String modelNumber = " Aavante Bar 500 / Aavante";
+				String color = "White";
+				String type = "SoundBar";
+				String blutooth = "Yes";
+				String salesPackage = "sound bar , Remote control, User manual, warranty card";
+				String powerOutPut = "10 w";
+				String wired = "Wired & Wireless";
+				
+				
+				
+				
+						System.out.println(" The model Number is " + modelNumber);
+						System.out.println("color is " + color);
+						System.out.println("type of sound bar is " + type);
+						System.out.println("blutooth " + blutooth);
+						System.out.println("salesPackage " + salesPackage);
+						System.out.println("powerOutPut is " + powerOutPut);
+						System.out.println("it is " + wired);
+
+		
+		
+		
+				
+		
+		
+		
+		
+		}
+}*/
+
+
+
+/*class SoundBar{
+
+
+	//public static void main(String args[]){
+	
+
+		static		String modelNumber = " Aavante Bar 500 / Aavante";
+		static		String color = "White";
+		static		String type = "SoundBar";
+		static		String blutooth = "Yes";
+		static		String salesPackage = "sound bar , Remote control, User manual, warranty card";
+		static		String powerOutPut = "10 w";
+		static		String wired = "Wired & Wireless";
+				
+							public static void main(String args[]){
+				
+				
+						System.out.println(" The model Number is " + modelNumber);
+						System.out.println("color is " + color);
+						System.out.println("type of sound bar is " + type);
+						System.out.println("blutooth " + blutooth);
+						System.out.println("salesPackage " + salesPackage);
+						System.out.println("powerOutPut is " + powerOutPut);
+						System.out.println("it is " + wired);
+
+		
+		
+		
+				
+		
+		
+		
+		
+		}
+}*/
+
+
+
+
+class SoundBar{
+
+
+	//public static void main(String args[]){
+	
+
+		static		String modelNumber ;//= " Aavante Bar 500 / Aavante";
+		static		String color ;//= "White";
+		static		String type ;//= "SoundBar";
+		static		String blutooth;// = "Yes";
+		static		String salesPackage ;//= "sound bar , Remote control, User manual, warranty card";
+		static		String powerOutPut ;//= "10 w";
+		static		String wired ;//= "Wired & Wireless";
+				
+							public static void main(String args[]){
+				
+				
+						System.out.println(" The model Number is " + modelNumber);
+						System.out.println("color is " + color);
+						System.out.println("type of sound bar is " + type);
+						System.out.println("blutooth " + blutooth);
+						System.out.println("salesPackage " + salesPackage);
+						System.out.println("powerOutPut is " + powerOutPut);
+						System.out.println("it is " + wired);
+
+		
+		
+		
+				
+		
+		
+		
+		
+		}
+}
