fbabf21139093f5f2a61749e3ab30a1409c7e8c7
siddu9900 <siddu.xworkz@gmail.com>
2023-08-23 09:55:57 +0530

    Method is commit

---
+++ Additions.java
@@ -0,0 +1,94 @@
+/*class Additions{
+
+
+	public static void main(String args[]){
+	
+		add(45,67);
+		add(54,96);
+		add(12,63);
+		add(24,60);
+		add(16,42);
+		add(450,630);
+		add(978,632);
+		add(78,98);
+		add(6123,36245);
+		add(55,68);
+		
+		
+	}
+		
+		
+						public static void  add(int a, int b){
+					
+				
+				
+				System.out.println(a+b);
+				
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+}*/
+
+
+
+
+
+
+
+
+
+
+/*class Addition{
+	
+		public static void main(String args[]){
+			add(1,8);
+			add(23,63);
+			
+			}
+	
+			public static  void add( int a, int b){
+				System.out.println(a+b);
+	
+	}
+	
+	
+}*/
+
+
+class Additions{
+	public static void main(String args []){
+		add(1,8);
+		add(8,9);
+	}
+	
+	public static void add(int a, int b){
+		
+		System.out.println(a+b);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Multiplication.java
@@ -0,0 +1,27 @@
+class Multiplication{
+
+
+		public static void main(String args[]){
+		
+			multi(12,23);
+			multi(56,65);
+			multi(90,80);
+			multi(70,40);
+			multi(55,48);
+			multi(78,45);
+			multi(15,64);
+			multi(45,80);
+			multi(74,30);
+			multi(87,85);
+		
+		
+		}
+		
+		
+			public static void multi(int a, int b){
+			
+				System.out.println(a*b);
+			}
+		
+
+}
